d03cc773134397e3286ba537f81edd6fae39888c
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2023-03-20 16:49:10 +0530

    .

---
+++ 04/04_escape_sequences.py
@@ -0,0 +1,26 @@
+# Hi Devanshu plesse don't remove below lines
+
+print("Hello world")
+print("hello\nworld")
+
+# seperator sep concates the comma sepereated strings with given seperator and if not given then sep = " "
+print("Devanshu", "K", "Shah", sep=",")
+
+
+'''
+
+multiline
+comments 
+are
+here
+
+'''
+
+
+# escape sequences
+
+print("\"Hiii\"")
+print('"Double quotes inside single quotes"')
+print("'Single Quotes inside double quotes'")
+print("a", 6, 7, end="009\n")
+print("ha haa haaa")
---
+++ 04/05_datatypes_and_variables.py
@@ -0,0 +1,36 @@
+a = 20
+print("Type of a is -> ", type(a))
+
+b = 40.2
+print("Type of b is -> ", type(b))
+
+c = "Devanshu"
+print("Type of c is -> ", type(c))
+
+d = True
+print("Type of d is -> ", type(d))
+
+e = None
+print("Type of e is -> ", type(e))
+
+f = 'a'
+print("Type of f is -> ", type(f))
+
+g = complex(8, 2)
+
+print("Type of g is -> ", type(g))
+
+print(a+b)
+# print(b+c) will generate an error as string and number cant be added
+
+list = ["Devanshu", "Shah", 4, 21, True]
+print("The type of list is -> ", type(list))
+
+set = {"Devanshu", "Shah", 4, 21, True}
+print("The type of map is ->", type(set))
+
+dict = {"Name": "Devanshu", "age": 21, "sem": 4}
+print("the type of dict is ", type(dict))
+
+tuple = ("Devanshu", "Shah", 4, 21, True)
+print("the type of tuple is ", type(tuple))
