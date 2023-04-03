c92ae06804e4097bb9fd6a1a927aaeded9f8d879
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2023-04-03 21:11:39 +0530

    Create 20_functions.py

---
+++ 03/20_functions.py
@@ -0,0 +1,26 @@
+def geometricMean(a, b):
+    return (a*b)/(a+b)
+
+
+print(geometricMean(2, 3))
+
+
+def greater(a, b):
+    if (a > b):
+        print(a)
+    else:
+        print(b)
+
+
+greater(2, 3)
+
+# also we can use pass in the place of body
+# example
+
+
+def isLesser(a, b):
+    pass
+
+# 2 types of functions
+# Built in functions
+# User defined functions
